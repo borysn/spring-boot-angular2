6655a4f2988d3cc1acdd6eeb6b0b4a8988e72dc8
Borys Niewiadomski <xborysn@gmail.com>
2016-04-26 12:00:10 -0500

    update angular2

--- src/main/web/app/main.ts
+++ src/main/web/app/main.ts
@@ -2,9 +2,8 @@
 
 import {provide} from 'angular2/core';
 import {bootstrap}    from 'angular2/platform/browser';
-import {ROUTER_PROVIDERS,
-        LocationStrategy,
-        HashLocationStrategy} from 'angular2/router';
+import {ROUTER_PROVIDERS} from 'angular2/router';
+import {LocationStrategy, HashLocationStrategy} from 'angular2/platform/common';
 import {HTTP_PROVIDERS} from 'angular2/http';
 
 import {AppComponent} from './app.component';
