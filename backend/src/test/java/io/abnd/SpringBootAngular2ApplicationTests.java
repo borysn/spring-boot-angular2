ab178821926213a98b251c22babd3632efb8ed26
borysn <xborysn@gmail.com>
2016-08-30 20:00:27 -0500

    spring-boot 1.4 updates

--- backend/src/test/java/io/abnd/SpringBootAngular2ApplicationTests.java
+++ backend/src/test/java/io/abnd/SpringBootAngular2ApplicationTests.java
@@ -2,13 +2,11 @@ package io.abnd;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.test.context.web.WebAppConfiguration;
-import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@SpringApplicationConfiguration(classes = SpringBootAngular2Application.class)
-@WebAppConfiguration
+@SpringBootTest
 public class SpringBootAngular2ApplicationTests {
 
 	@Test
